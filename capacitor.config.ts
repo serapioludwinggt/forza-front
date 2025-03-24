5be2efb5baaf28397012155088900870110ce3e3
Lud Serapio <serapio.ludwing.gt@gmail.com>
2025-03-24 09:01:41 -0600

    fix errors compilation android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.forzafront.com',
   appName: 'forza-front',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'http',
+    cleartext: true
+  },
+  android: {
+    allowMixedContent: true
+  },
 };
 
 export default config;
